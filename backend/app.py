b80e6a38c73729e79cdcf12e27769ac3d4833a45
01stella <stellaagwn@gmail.com>
2026-05-12 00:48:40 +0700

    added hp tracking, auto scroll logs, character selection

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,8 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 # GAME STATE
 connected_players = {}
 player_characters = {} # tracks who picks what character
+current_wagers = {} # RPS choice tracker
+current_hp = {}
 
 CHARACTER_STATS = {
     'Aha': {'hp': 1000, 'dmg': 130, 'speed': 60},
@@ -57,10 +59,29 @@ def handle_character_selection(data):
 
     if user_role in ['Player1', 'Player2']:
         character_name = data.get('character')
+
+        if user_role in player_characters:
+            print(f"{user_role} attempted to change character from {player_characters[user_role]} to {character_name}")
+            return
+
         if character_name in CHARACTER_STATS:
             player_characters[user_role] = character_name
             print(f"{user_role} selected character: {character_name}")
             emit('game_update', {'message': f'{user_role} selected {character_name}'}, broadcast=True)
+
+            if len(player_characters) == 2:
+                print(f"Both players have selected characters: {player_characters}")
+                emit('game_update', {'message': f"Both players have selected characters: {player_characters}"}, broadcast=True)
+
+                p1_char = player_characters['Player1']
+                p2_char = player_characters['Player2']
+
+                current_hp['Player1'] = CHARACTER_STATS[p1_char]['hp']
+                current_hp['Player2'] = CHARACTER_STATS[p2_char]['hp']
+
+                emit('match_start', {
+                    'message': 'Match is starting!',
+                    'hp_data': current_hp }, broadcast=True)
         else:
             print(f"{user_role} attempted to select an invalid character: {character_name}")
     else:
@@ -69,10 +90,52 @@ def handle_character_selection(data):
 @socketio.on('player_action')
 def handle_player_action(data):
     session_id = request.sid
-    user_role = connected_players.get(session_id, 'Unknown')
+    attacker = connected_players.get(session_id, 'Unknown')
 
-    print(f"Received action from {user_role} ({session_id}): {data}")
-    emit('game_update', {'message': f'{user_role} performed an action: {data}'}, broadcast=True)
+    if attacker in ['Player1', 'Player2']:
+        defender = 'Player2' if attacker == 'Player1' else 'Player1'
+
+        if current_hp.get(attacker, 0) <= 0 or current_hp.get(defender, 0) <= 0:
+            return
+
+        attacker_char = player_characters.get(attacker)
+        damager = CHARACTER_STATS[attacker_char]['dmg']
+
+        current_hp[defender] -= damager
+
+        if current_hp[defender] < 0:
+            current_hp[defender] = 0
+
+        emit('game_update', {'message': f'{attacker} ({attacker_char}) performed an action: {data} and dealt {damager} damage!'}, broadcast=True)
+
+        if current_hp[defender] == 0:
+            emit ('game_update', {'message': f'{defender} has been defeated! {attacker} wins!'}, broadcast=True)
+
+            emit('match_end', {'message': f'Match ended! {attacker} wins!'}, broadcast=True)
+        
+        
+@socketio.on('submit_wager')
+def handle_wager(data):
+    session_id = request.sid
+    user_role = connected_players.get(session_id, 'Unknown')
+    
+    if user_role in ['Player1', 'Player2']:
+        wager = data.get('wager') # 'Rock', 'Paper', or 'Scissors'
+        
+        # Save their bet in the vault
+        current_wagers[user_role] = wager
+        print(f"{user_role} locked in a blind wager.")
+        
+        # Tell their specific frontend that the bet was received
+        emit('wager_received', {'message': 'Wager locked! Waiting for opponent...'}, to=session_id)
+        
+        if len(current_wagers) == 2:
+            print(f"Clash ready! P1: {current_wagers.get('Player1')} vs P2: {current_wagers.get('Player2')}")
+            
+            # math here later to determine
+            
+            # Clear the vault for the next round
+            current_wagers.clear()
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, use } from 'react';
 import { io } from 'socket.io-client';
 
 // Connecting to backend
@@ -7,7 +7,15 @@ const socket = io('http://localhost:5000');
 function App() {
   const [playerRole, setPlayerRole] = useState('Connecting...')
   const [logs, setLogs] = useState([])
+  const logsEndRef = useRef(null)
+  const [gameStarted, setGameStarted] = useState(false)
+  const [lockedCharacter, setLockedCharacter] = useState(null)
+  const [winner, setWinner] = useState(null)
 
+  useEffect(() => {
+        logsEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }, [logs])
+  
   useEffect(() => {
     socket.on('role_assignment', (data) => {
       setPlayerRole(data.role)
@@ -17,10 +25,21 @@ function App() {
       setLogs((prevLogs) => [...prevLogs, data.message])
     })
 
+    socket.on('match_start', (data) => {
+      setGameStarted(true)
+      setLogs((prevLogs) => [...prevLogs, data.message, 'The match has started!'])
+    })
+
+    socket.on('match_end', (data) => {
+      setWinner(data.message.split('!')[0]) // Extract the winner's name
+    })
+
     // Clean up the socket connection on unmount
     return () => {
       socket.off('role_assignment')
       socket.off('game_update')
+      socket.off('match_start')
+      socket.off('match_end')
     }
   }, [])
 
@@ -30,14 +49,17 @@ function App() {
 
   const handleSelectCharacter = (character) => {
     socket.emit('character_selection', { character })
+    setLockedCharacter(character)
   };
 
   return (
     <div style={{ padding: '20px' }}>
       <h1>Aetherium Clash</h1>
       <p> Role: {playerRole}</p>
-    
-      {playerRole !== 'Spectator' && (
+      <p> Locked Character: {lockedCharacter}</p>
+
+      {/* CHARACTER SELECTION PHASE */}
+      {playerRole !== 'Spectator' && !gameStarted && !lockedCharacter && (
         <div style={{ marginBottom: '20px' }}>
           <h2>Select Your Character:</h2>
           <div style={{ display: 'flex', gap: '10px', marginBottom: '20px' }}>
@@ -45,14 +67,37 @@ function App() {
             <button onClick={() => handleSelectCharacter('Lan')}>Lan</button>
             <button onClick={() => handleSelectCharacter('Yaoshi')}>Yaoshi</button>
           </div>
-          
-          <button onClick={handleAttack}>Attack</button>
+        </div>
+      )}
+
+      {/* WAITING ROOM */}
+      {playerRole !== 'Spectator' && !gameStarted && lockedCharacter && (
+        <div style={{ marginBottom: '20px' }}>
+          <h2>Waiting for Other Players...</h2>
+          <p> You have selected <strong style={{ color: 'blue' }}>{lockedCharacter}</strong></p>
+        </div>
+      )}
+
+      {/* COMBAT PHASE */}
+      {playerRole !== 'Spectator' && gameStarted && !winner && (
+        <div style={{ marginBottom: '20px' }}>
+          <h2>Combat Phase</h2>
+          <button onClick={handleAttack}>Basic Attack</button>
+        </div>
+      )}
+
+      {/* GAMEOVER PHASE */}
+      {winner && (
+        <div style={{ marginBottom: '20px' }}>
+          <h2>Game Over</h2>
+          <p>The winner is: <strong style={{ color: 'green' }}>{winner}</strong></p>
         </div>
       )}
 
       <h2 style={{ marginTop: '20px' }}>Battle Logs:</h2>
-      <ul style={{ background: '#f0f0f0', padding: '10px', borderRadius: '5px', listStyleType: 'none', color: 'black' }}>
+      <ul style={{ background: '#f0f0f0', padding: '10px', borderRadius: '5px', listStyleType: 'none', color: 'black', 'height': '250px', 'overflowY': 'auto' }}>
         {logs.map((log, index) => <li key={index} style={{ marginBottom: '5px' }}>{log}</li>)}
+        <div ref={logsEndRef} />
       </ul>
     </div>
   );
